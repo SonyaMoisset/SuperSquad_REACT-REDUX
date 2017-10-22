21570271655762783d6d859dd9741b1cd8f13c6f
smoisset <smoisset@worldremit.com>
2017-10-22 22:41:51 +0100

    Create store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer)
+console.log('store', store)
+console.log('getstate', store.getState())
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 )
