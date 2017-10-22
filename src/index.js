1a227f88d3920398e648639d69c2ce6e73456ad3
smoisset <smoisset@worldremit.com>
2017-10-22 22:50:42 +0100

    Add first action

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import rootReducer from './reducers'
+import { addCharacterById } from './actions'
 
 const store = createStore(rootReducer)
-console.log('store', store)
-console.log('getstate', store.getState())
+store.subscribe(() => console.log('store', store.getState()))
+store.dispatch(addCharacterById(2))
 
 ReactDOM.render(
     <Provider store={store}>
